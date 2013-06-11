d52489a1403aae8fd9583a9fd92a88c2a8913854
senquevila <senquevila@gmail.com>
2013-06-11 05:52:05 -0600

    commit inicial

---
+++ app-connect.js
@@ -0,0 +1,23 @@
+var connect = require('connect');
+var connectRoute = require('connect-route');
+var htutil = require('./htutil');
+
+
+connect.createServer()
+.use(connect.favicon())
+.use(connect.logger())
+.use('/filez', connect.static(__dirname + '/filez'))
+.use(connectRoute (function(app){
+	app.get('/',
+	require('./home-node').get);
+	app.get('/square', htutil.loadParams,
+	require('./square-node').get);
+	app.get('/factorial', htutil.loadParams,
+	require('./factorial-node').get);
+	app.get('/fibonacci', htutil.loadParams,
+	require('./fibo2-node').get);
+	app.get('/mult', htutil.loadParams,
+	require('./mult-node').get);
+})).listen(8124);
+
+console.log('listening to http://localhost:8124');
